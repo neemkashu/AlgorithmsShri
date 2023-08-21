dc76abb12951a1d03b8998602bae6c7091d45de2
neemkashu <sparjinkust@mail.ru>
2023-08-21 21:55:36 +0300

    binary

--- problems/binary-search/binary-search.js
+++ problems/binary-search/binary-search.js
@@ -1,5 +1,17 @@
 // https://en.wikipedia.org/wiki/Binary_search_algorithm
 
 export function binarySearch(arr, value) {
+  let left = 0;
+  let right = arr.length;
 
+  while ( left < right) {
+    let m = Math.ceil((left + right) / 2)
+    if (arr[m] < value) {
+      left = m;
+    } else {
+      right = m - 1;
+    }
+  }
+
+  return arr[left] === value ?  left : arr[left + 1] === value ? left + 1 : undefined;
 }
