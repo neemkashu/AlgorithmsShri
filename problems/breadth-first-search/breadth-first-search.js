84bdbe5d643c0ba4c2ec078661f718e5acce706d
neemkashu <sparjinkust@mail.ru>
2023-08-21 19:58:32 +0300

    valid parenthesis

--- problems/breadth-first-search/breadth-first-search.js
+++ problems/breadth-first-search/breadth-first-search.js
@@ -8,6 +8,36 @@
  *     this.right = (right===undefined ? null : right)
  * }
  */
+// class Queue {
+//     constructor() {
+//         this.stackOne = []
+//         this.stackTwo = []
+//     }
+//     push(value) {
+//         this.stackOne.push(value)
+//     }
+//     pop() {
+//         if (this.stackTwo.length < 1) {
+//             while (this.stackOne.length > 0) {
+//                 this.stackTwo.push(this.stackOne.pop())
+//             }
+//         }
+//         return this.stackTwo.pop()
+//     }
+// }
+// export function bfs(tree) {
+//     const result = [];
+//     const queue = new Queue();
+//     queue.push(tree)
+// 
+//     while(queue.length > 0) {
+//         const node = queue.pop()
+//         result.push(node.val)
+//         if (node.left) queue.push(node.left)
+//         if (node.right) queue.push(node.right)
+//     }
+//     return result;
+// }
 
 export function bfs(tree) {
     const result = [];
--- problems/valid-parenthesis/valid-parenthesis.js
+++ problems/valid-parenthesis/valid-parenthesis.js
@@ -1,5 +1,21 @@
 // https://leetcode.com/problems/valid-parentheses/
 
-export function isValid(str) {
+const isOpeningBrace = (char) => ['(', '{', '['].includes(char)
+const isOfSameType = (braceOpen, braceClose) => ['()', '{}', '[]'].includes(braceOpen + braceClose)
 
+export function isValid(str) {
+  const stack = [];
+
+  for (let index = 0; index < str.length; index++) {
+    const brace = str[index];
+    if (isOpeningBrace(brace)) {
+      stack.push(brace)
+    } else {
+      const braceLeft = stack.pop()
+      if (!isOfSameType(braceLeft, brace)) {
+        return false;
+      }
+    }
+  }
+  return true;
 }
