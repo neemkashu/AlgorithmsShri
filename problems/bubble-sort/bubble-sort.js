63710efc4c5b55bb5f731cf1fff08eb2264c4b4a
neemkashu <sparjinkust@mail.ru>
2023-08-21 18:32:29 +0300

    bubble-sort

--- problems/bubble-sort/bubble-sort.js
+++ problems/bubble-sort/bubble-sort.js
@@ -1,7 +1,24 @@
 // https://sortvisualizer.com/bubblesort/
 // https://en.wikipedia.org/wiki/Bubble_sort
+const swap = (i,j,arr) => {
+    const temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp;
+}
 
 export function bubbleSort(arr) {
+    let swapAmount = 0;
 
+    do {
+        swapAmount = 0;
+        for (let index = 1; index < arr.length; index++) {
+            const element = arr[index];
+            const elementPrev = arr[index - 1]
+            if (elementPrev > element) {
+                swap(index, index-1, arr)
+                swapAmount ++;
+            }
+        }
+    } while (swapAmount > 0);
     return arr;
 }
