1337d23b4915fc8ecc4fcd74263229f340326db1
neemkashu <sparjinkust@mail.ru>
2023-08-21 20:26:30 +0300

    prime

--- problems/prime-numbers/prime-numbers.js
+++ problems/prime-numbers/prime-numbers.js
@@ -1,7 +1,16 @@
 // https://ru.wikipedia.org/wiki/Решето_Эратосфена
 
 export function primeNumbers(num) {
-    const result = [];
+    const result = Array.from({length: num + 1}).fill(0).map((_, i) => i)
 
-    return result;
+    let devider = 2;
+    while (devider ** 2 < num) {
+        for (let index = devider * 2; index < result.length; index += devider) {
+            result[index] = -1;
+        }
+        devider ++;
+    }
+
+    const final = result.filter((element, index) => index >= 2 && element > 0);
+    return final
 }
