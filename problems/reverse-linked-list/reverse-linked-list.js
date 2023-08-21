1bcfc109657893ab2e4172897ffc04d8b10562fd
neemkashu <sparjinkust@mail.ru>
2023-08-21 21:28:25 +0300

    reverse list

--- problems/reverse-linked-list/reverse-linked-list.js
+++ problems/reverse-linked-list/reverse-linked-list.js
@@ -3,11 +3,23 @@
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
+ *     this.val = (val===undefined ? 0 : v6al)
  *     this.next = (next===undefined ? null : next)
  * }
  */
 
 export function reverseList(head) {
+  let prev = null;
+  let curr = head;
+  let next = head.next;
+
+  while(next) {
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+    next = next.next;
+  }
+  curr.next = prev;
+  return curr;
 
 }
